c6ee4bcdc091d5fc9ccaef8f0043f6eace10cd31
2022BCD0052 <yogesh22bcd52@iiitkottayam.ac.in>
2025-02-28 23:49:17 +0530

    Add initial implementation of Prisma client, utility functions, and UI components

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import { BlogFooter } from "@/components/home/blog-footer";
+import { Navbar } from "@/components/home/header/navbar";
+import HeroSection from "@/components/home/hero-section";
+import { TopArticles } from "@/components/home/top-articles";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+   <Navbar />
+   <HeroSection/>
+   <TopArticles/>
+   <BlogFooter/>
+      </div>
+  );
+}
